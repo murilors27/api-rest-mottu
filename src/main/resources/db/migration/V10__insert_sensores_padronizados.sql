725e08a62c89868293ad9a62f56fd6bf26ce096f
murilors27 <murilorsantos27@gmail.com>
2025-11-09 09:13:01 -0300

    fix: evita conflito de PK no insert de sensores padronizados

--- src/main/resources/db/migration/V10__insert_sensores_padronizados.sql
+++ src/main/resources/db/migration/V10__insert_sensores_padronizados.sql
@@ -1,14 +1,14 @@
-ALTER SEQUENCE sensores_id_seq RESTART WITH 1;
-
-INSERT INTO sensores (localizacao) VALUES
-('Setor A - Coluna 1'),
-('Setor B - Coluna 2'),
-('Setor C - Coluna 3'),
-('Setor D - Coluna 4'),
-('Setor E - Coluna 5'),
-('Setor F - Coluna 6'),
-('Setor G - Coluna 7'),
-('Setor H - Coluna 8'),
-('Setor I - Coluna 9');
+INSERT INTO sensores (localizacao)
+VALUES
+    ('Setor A - Coluna 1'),
+    ('Setor B - Coluna 2'),
+    ('Setor C - Coluna 3'),
+    ('Setor D - Coluna 4'),
+    ('Setor E - Coluna 5'),
+    ('Setor F - Coluna 6'),
+    ('Setor G - Coluna 7'),
+    ('Setor H - Coluna 8'),
+    ('Setor I - Coluna 9')
+ON CONFLICT DO NOTHING;
 
 SELECT setval('sensores_id_seq', (SELECT MAX(id) + 1 FROM sensores));
